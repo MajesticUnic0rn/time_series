259f1f372e889e7f451e9487cbdffaf9093109da
MajesticUnic0rn <chrisdong08@gmail.com>
2022-05-30 00:51:33 -0400

    changing workflow to src for standards

---
+++ src/data_process.py
@@ -0,0 +1,23 @@
+# import packages
+
+
+# import raw data
+
+
+
+# import data config 
+#cat variable
+#numerical variable
+#label column
+
+
+# clean information
+
+
+# if dev
+# export train-predict - 
+
+#if prod 
+#export prediction dataframe???
+
+# metadata - cleaning
---
+++ util/anon.py
@@ -0,0 +1,10 @@
+# import packages
+
+
+# take picture of lava lamp, has value and use it as seed
+
+# function to convert string variables to hash function
+
+
+
+# function to convert numerical random by multiplication
